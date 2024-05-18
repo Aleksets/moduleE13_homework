0a3748a13c47c5ac433de0efaf3f55fde0aa4acc
Alexey Zaplatin <a.g.zaplatin@mail.ru>
2024-05-19 01:44:41 +0300

    Commit 7 - Linter test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import * as math from './math.js';
+import * a math from './math.js';
 import './style.css';
 
 const multiply = (a) => a * 5;
 
-console.log('math.sum 2+3= ' + math.sum(2, 3));
+console.log('math.sum 2+3= ' + math.sum(2 3));
 console.log('multiply 2*5= ' + multiply(2));
 console.log('math.multiply 2*Pi= ' + math.multiply(2));
 // document.write('Hello, мир!!!!');
